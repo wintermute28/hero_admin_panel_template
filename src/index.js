4db9bba09a5d27a501adeb00aa6db934e57cf880
wintermute28 <a28c@yandex.ru>
2024-03-17 11:49:13 +0300

    Initial repo

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import App from './components/app/App';
+import store from './store';
+
+import './styles/index.scss';
+
+ReactDOM.render(
+
+    <Provider store={store}>
+      <App />
+    </Provider>
+
+  ,
+  document.getElementById('root')
+);
+
---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import { configureStore } from '@reduxjs/toolkit';
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
+
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action);
+};
+
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({
+//                 type: action
+//             })
+//         }
+//         return oldDispatch(action);
+//     }
+//     return store;
+// }
+
+// const store = createStore(
+//                 combineReducers({heroes, filters}),
+//                 compose(applyMiddleware(thunk, stringMiddleware),
+//                         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//                 );
+
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
+})
+
+export default store;
